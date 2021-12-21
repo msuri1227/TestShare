a3bf184f78eaeb49690599059b5b700f39f48460
MSURI <msuri@ondevice.co.uk>
2021-12-21 20:58:59 +0530

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,5 @@ $("#login-button-Clariant").click(function(event){
 	window.open("itms-services://?action=download-manifest&url=https://dl.dropboxusercontent.com/s/0jco26xf8ikx8sf/manifest.plist")
 });
 $("#login-button-Heritage").click(function(event){
-    window.open("itms-services://?action=download-manifest&url=https://dl.dropboxusercontent.com/s/0jco26xf8ikx8sf/manifest.plist")
+    window.open("itms-services://?action=download-manifest&url=https://dl.dropboxusercontent.com/s/2lqo4uqw7h3n6lx/manifest.plist")
 });
