b1c231dd340825a9ec9dde444573a3ea15305bd1
MSURI <msuri@ondevice.co.uk>
2021-12-21 18:53:09 +0530

    publish

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 $("#login-button").click(function(event){
-	window.open("itms-services://?action=download-manifest&url=https://dl.dropboxusercontent.com/s/0jco26xf8ikx8sf/manifest.plist")
+	window.open("itms-services://?action=download-manifest&url=https://dl.dropboxusercontent.com/s/0jco26xf8ikx8sf/manifest.plist","_self")
 });
